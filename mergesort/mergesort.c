27c632db5ae09f9f5fe62f2e6a925d4e87218da9
Lloyd Hilsgen <hilsg008@zytel.morris.umn.edu>
2022-10-24 12:59:34 -0500

    initial commit

--- mergesort/mergesort.c
+++ mergesort/mergesort.c
@@ -1,12 +1,13 @@
 #include "mergesort.h"
 
 void mergesort(int size, int values[]) {
-  // This obviously doesn't actually do any *sorting*, so there's
-  // certainly work still to be done.
-  //
-  // Remember that a key goal here is to learn to use
-  // `malloc/calloc` and `free`, so make sure you explicitly
-  // allocate any new arrays that you need, even if you
-  // might not strictly need to.
-  return;
+  mergesort_range(0, size, values);
+}
+
+void mergesort_range(int start_index, int end_index, int[] values) {
+  int rangesize = 
+}
+
+void merge_ranges(int start_index, int midpoint, int end_index, int values[]) {
+  
 }
--- mergesort/mergesort.h
+++ mergesort/mergesort.h
@@ -7,4 +7,8 @@
 
 void mergesort(int size, int values[]);
 
+void mergesort_range(int start_index, int end_index, int[] values);
+
+void merge_ranges(int start_index, int midpoint, int end_index, int values[]);
+
 #endif
